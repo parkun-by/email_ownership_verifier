8d70f04890e8a412db2cb13986e9ecafefe2dc66
Andrei Dziaineka <dziaineka@gmail.com>
2019-12-26 20:59:23 +0300

    logging and annotations

--- verifier.py
+++ verifier.py
@@ -1,13 +1,14 @@
 import random
+from typing import Tuple
 import mailer
 import config
 
 
-def get_secret_code():
+def get_secret_code() -> int:
     return random.randint(1000000, 9999999)
 
 
-def get_parameters(raw_parameters):
+def get_parameters(raw_parameters: dict) -> Tuple[str, str]:
     email = ''
     language = config.BY
 
@@ -21,7 +22,7 @@ def get_parameters(raw_parameters):
     return email, language
 
 
-def send_verification_mail(raw_parameters):
+def send_verification_mail(raw_parameters: dict) -> str:
     email, language = get_parameters(raw_parameters)
 
     if not email:
@@ -30,6 +31,8 @@ def send_verification_mail(raw_parameters):
     secret_code = get_secret_code()
 
     if mailer.send_mail(email, secret_code, language) in config.SUCCESS_CODES:
+        print(f"email: {email}, code: {str(secret_code)}")
         return str(secret_code)
     else:
+        print(f"email: {email}, fail")
         return config.FAIL
